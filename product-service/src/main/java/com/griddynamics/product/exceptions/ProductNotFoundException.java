cd8ba5369eff66d24ecf8ef5c8df9dacfab0778f
Roman Polosmak <roman.polosmak@advance-auto.com>
2025-07-18 15:13:15 +0300

    Revert "Added distributed systems and microservices architecture project"
    This reverts commit 2e95263a4f43d06b802be83636063f6875b594e0.

--- product-service/src/main/java/com/griddynamics/product/exceptions/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.griddynamics.product.exceptions;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
